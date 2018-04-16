1799cc22f00e4f1e6ba8f0442f54f32135ac6272
Kiah Tolliver <adamskiah@gmail.com>
2018-04-16 19:48:26 -0400

    processor setup

---
+++ CannaTraxx.Common/IOC/CommonProcessor.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using CannaTrax.Data.EF.IOC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CannaTraxx.Common.IOC
+{
+   public class CommonProcessor : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new DataModule());
+            builder.RegisterModule(new ProcessorModule());
+        }
+    }
+}
---
+++ CannaTraxx.Common/IOC/ProcessorModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using CannaTraxx.Common.Processors;
+
+namespace CannaTraxx.Common.IOC
+{
+    internal class ProcessorModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CustomerProcessor>().As<ICustomerProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<CategoryProcessor>().As<ICategoryProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<GeneralSettingsProcessor>().As<IGeneralSettingsProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<ItemProcessor>().As<IItemProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<ModuleProcessor>().As<IModuleProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<PurchaseProcessor>().As<IPurchaseProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<PurchaseDetailProcessor>().As<IPurchaseDetailProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<RoleProcessor>().As<IRoleProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<SaleProcessor>().As<ISaleProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<SaleDetailProcessor>().As<ISaleDetailProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<SalePaymentProcessor>().As<ISalePaymentProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<ShopProcessor>().As<IShopProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<SupplierProcessor>().As<ISupplierProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<TaxProcessor>().As<ITaxProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<UserProcessor>().As<IUserProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<UserLogProcessor>().As<IUserLogProcessor>().InstancePerLifetimeScope();
+            builder.RegisterType<UserPermissionProcessor>().As<IUserPermissionProcessor>().InstancePerLifetimeScope();
+
+        }
+    }
+}
